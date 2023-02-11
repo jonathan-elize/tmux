fc3bc83001b9d1dc16ca5895ffb09c2ca4e70a28
Jonathan Elize <jonathan.elize@gmail.com>
2023-02-10 18:58:27 -0600

    fix(tmux): correct issue with battery component not showing percentage for mac

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -41,7 +41,7 @@ battery_percent()
   case $(uname -s) in
     Linux)
       percent=$(linux_acpi percent)
-      [ -n "$percent" ] && echo " $percent"
+      [ -n "$percent" ] && echo " $percent%"
       ;;
 
     Darwin)
@@ -116,11 +116,11 @@ main()
   bat_perc=$(battery_percent)
 
   if [ -z "$bat_stat" ]; then # Test if status is empty or not
-    echo "$bat_label$bat_perc%"
+    echo "$bat_label$bat_perc"
   elif [ -z "$bat_perc" ]; then # In case it is a desktop with no battery percent, only AC power
     echo "$bat_label$bat_stat"
   else
-    echo "$bat_label$bat_stat$bat_perc%"
+    echo "$bat_label$bat_stat$bat_perc"
   fi
 }
 
